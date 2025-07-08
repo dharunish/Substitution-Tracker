b64abb29944a6154d3056f9be90780647847e64b
Dharunish <dharyugi@gmail.com>
2025-07-08 17:15:15 -0400

    Initial Commit

---
+++ Substitution Tracker/Substitution_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Substitution_TrackerApp.swift
+//  Substitution Tracker
+//
+//  Created by Dharunish Yugeswardeenoo on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Substitution_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
